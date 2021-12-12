01475267870183acdba6f3a1c9e88de214fcf565
BuildTools <unconfigured@null.spigotmc.org>
2021-12-12 16:28:35 +0500

    First commit

---
+++ src/JDBCPostgres.java
@@ -0,0 +1,61 @@
+import java.math.BigDecimal;
+import java.sql.*;
+
+public class JDBCPostgres {
+
+    private final String DB_URL;
+    private final String USER;
+    private final String PASS;
+    private Connection connection;
+
+    public JDBCPostgres() {
+        DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+        USER = "postgres";
+        PASS = "";
+    }
+
+    public JDBCPostgres(String dbUrl, String user, String pass) {
+        DB_URL = dbUrl;
+        USER = user;
+        PASS = pass;
+    }
+
+    public void createConnection() {
+        if (driverIsFound()) {
+            try {
+                this.connection = DriverManager
+                        .getConnection(DB_URL, USER, PASS);
+            } catch (SQLException e) {
+                System.out.println("Connection Failed");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private boolean driverIsFound() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            return true;
+        } catch (ClassNotFoundException e) {
+            System.out.println("PostgreSQL JDBC Driver is not found. Include it in your library path ");
+            return false;
+        }
+    }
+
+    public void getEmployeeList() {
+        String SQL_SELECT = "SELECT * FROM java_task.employee";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL_SELECT);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                long id = resultSet.getLong("ID");
+                String name = resultSet.getString("NAME");
+                BigDecimal salary = resultSet.getBigDecimal("SALARY");
+                Timestamp createdDate = resultSet.getTimestamp("CREATED_DATE");
+                System.out.println(id + ", " + name + ", " + salary + ", " + createdDate);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/JDBCPostgresTest.java
@@ -0,0 +1,13 @@
+//STEP 1. Import required packages
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class JDBCPostgresTest {
+
+    public static void main(String[] args) throws SQLException {
+        JDBCPostgres db = new JDBCPostgres();
+        db.createConnection();
+        db.getEmployeeList();
+    }
+}
